e897e86c8a2bba8a397cd6b30a4a86fa2f7f684d
Sushilkumar Pandey <s.pandey.ctr@ad.dot.gov>
2024-08-09 14:03:28 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace LinearSearch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myarray = { 1, 2, 3, 4, 5, 6, 7, 8 };
+            int n = 10;
+            Console.WriteLine(FindElementInArray(myarray, n));
+        }
+
+        static int FindElementInArray(int[] a, int n)
+        {
+            for(int i = 0; i < a.Length; i++) 
+            {
+                if (a[i] == n)
+                    return i;
+            }
+
+            return -1;
+        }
+    }
+}
